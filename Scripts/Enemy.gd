b969d78807c094a6dd766c9d08208abca541c550
adinh254 <adinh254@gmail.com>
2021-05-13 16:51:57 -0700

    added Enemy class

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+export var speed: float = 60.0
+var health: float = 100.0
+var target := Node2D.new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
