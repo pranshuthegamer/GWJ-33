a1cda532a406044b2a2d63257e31b74bb943dbde
PranshuTG <12345qwertyman12345@gmail.com>
2021-05-14 15:41:41 +0530

    Now all scenes need starting point

--- Scripts/Scene/all_scenes.gd
+++ Scripts/Scene/all_scenes.gd
@@ -7,6 +7,8 @@ onready var enemy = get_node("Enemy")
 func _ready() -> void:
 	enemy.connect("damage", self, "_on_Enemy_damage")
 	enemy.set_target(player)
+	#Every scene must have starting Point
+	Auto.character.transform = $StartingPoint.transform
 
 
 func _on_Enemy_damage(p_collider, p_damage):
