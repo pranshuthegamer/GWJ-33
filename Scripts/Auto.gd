a0c01853d62ddcea7397b88507e5de31d324f72b
PranshuTG <12345qwertyman12345@gmail.com>
2021-05-15 14:37:56 +0530

    .

--- Scripts/Auto.gd
+++ Scripts/Auto.gd
@@ -1,16 +1,13 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var Menu = preload("res://Scenes/Not Levels/Menu.tscn").instance()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$"/root/Main".add_child(Menu)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _cng_scene(scene,caller):
+	var tempscene = load(scene).instance()
+	caller.get_parent().add_child(tempscene)
+	caller.queue_free()
