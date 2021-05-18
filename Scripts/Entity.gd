3455e00ccb0f056a76d56df2633bd454fdcaef70
adinh254 <adinh254@gmail.com>
2021-05-17 19:37:42 -0700

    reworked player movement again

--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -1,33 +1,29 @@
 class_name Entity
-extends Area2D
+extends KinematicBody2D
 
 
-export var tile_size: int = 32
+export var speed: float = 200.0
 
 onready var sprite: Sprite = $Sprite
-onready var ray_up: RayCast2D = $RayCasts/Up
-onready var ray_down: RayCast2D = $RayCasts/Down
-onready var ray_left: RayCast2D = $RayCasts/Left
-onready var ray_right: RayCast2D = $RayCasts/Right
-
 var _direction := Vector2.ZERO
 
-#input
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_up") && !ray_up.is_colliding():
-		_direction += Vector2.UP
-	if event.is_action_pressed("ui_down") && !ray_down.is_colliding():
-		_direction += Vector2.DOWN
-	if event.is_action_pressed("ui_left") && !ray_left.is_colliding():
-		_direction += Vector2.LEFT
+
+func _process(_delta: float) -> void:
+	if _direction.x > 0:
 		sprite.scale.x = abs(sprite.scale.x)
-	if event.is_action_pressed("ui_right") && !ray_right.is_colliding():
-		_direction += Vector2.RIGHT
+	elif _direction.x < 0:
 		sprite.scale.x = -abs(sprite.scale.x)
-	set_physics_process(true)
 
 
 func _physics_process(_delta: float) -> void:
-	position += _direction * tile_size
 	_direction = Vector2.ZERO
-	set_physics_process(false)
+	if Input.is_action_pressed("ui_up"):
+		_direction += Vector2.UP
+	if Input.is_action_pressed("ui_down"):
+		_direction += Vector2.DOWN
+	if Input.is_action_pressed("ui_left"):
+		_direction += Vector2.LEFT
+	if Input.is_action_pressed("ui_right"):
+		_direction += Vector2.RIGHT
+	var new_velocity: Vector2 = _direction.normalized() * speed
+	move_and_slide(new_velocity)
