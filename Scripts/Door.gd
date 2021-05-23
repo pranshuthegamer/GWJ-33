26178ddedc83b5c8a282abe212ad3a131c9d7efd
adinh254 <adinh254@gmail.com>
2021-05-22 22:46:51 -0700

    changed collision layers and masks

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -7,6 +7,10 @@ onready var collider: CollisionShape2D = $Collider
 onready var door_sprite: Polygon2D = $Polygon2D
 
 
+func _ready() -> void:
+	set_process_unhandled_input(false)
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		is_open = !is_open
@@ -20,5 +24,5 @@ func _physics_process(_delta: float) -> void:
 	set_physics_process(false)
 
 
-func _on_InteractiveArea_state_change(p_interact_state: bool):
+func _on_InteractiveArea_state_change(p_interact_state: bool) -> void:
 	set_process_unhandled_input(p_interact_state)
